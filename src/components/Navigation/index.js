03fc0baabeeb95339784b93ec64d251ef9a94c48
Steve Gomez <StevenAGomez@Live.com>
2020-07-19 17:03:20 -0700

    Stubbed dialog

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -3,13 +3,19 @@ import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import IconButton from "@material-ui/core/IconButton";
-import Typography from "@material-ui/core/Typography";
 import Tooltip from "@material-ui/core/Tooltip";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import TwitterIcon from "@material-ui/icons/Twitter";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import Box from "@material-ui/core/Box";
 import Link from "@material-ui/core/Link";
+
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const useStyles = makeStyles((theme) => ({
   container: {
@@ -47,79 +53,115 @@ const useStyles = makeStyles((theme) => ({
 
 export default function PrimaryAppBar() {
   const classes = useStyles();
-  const doSomething = () => {
-    console.log("doSomething!");
+
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const descriptionElementRef = React.useRef(null);
+
+  const handleClickOpen = (e) => {
+    console.log("Here!", openDialog);
+    e.preventDefault();
+    setOpenDialog(true);
+  };
+
+  const handleClose = () => {
+    setOpenDialog(false);
   };
 
   const openExternalLink = (url) => {};
 
   return (
-    <Box className={classes.container}>
-      <Box className={classes.grow}>
-        <AppBar position="static">
-          <Toolbar>
-            <Box className={classes.menuSection}>
-              <Link color="secondary" href="#" onClick={doSomething}>
-                About
-              </Link>
-            </Box>
-            <Box className={classes.menuSection}>
-              <Link color="secondary" href="#" onClick={doSomething}>
-                FAQ
-              </Link>
-            </Box>
-            <Box className={classes.menuSection}>
-              <img
-                className={classes.logo}
-                src="images/logo-50.png"
-                alt="ChiliCheese.org"
-              />
-            </Box>
-            <Box className={classes.menuSection}>
-              <Link color="secondary" href="#" onClick={doSomething}>
-                Press
-              </Link>
-            </Box>
-            <Box className={classes.menuSection}>
-              <Box className={classes.sectionSocial}>
-                <Tooltip title="Follow us on Twitter">
-                  <IconButton
-                    aria-label="follow us on twitter"
-                    color="inherit"
-                    onClick={openExternalLink(
-                      "https://twitter.com/ChiliCheese_"
-                    )}
-                  >
-                    <TwitterIcon className={classes.icon} />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Join us on Facebook">
-                  <IconButton
-                    aria-label="join us on facebook"
-                    color="inherit"
-                    onClick={openExternalLink(
-                      "https://www.facebook.com/TheChiliCheeseBurrito"
-                    )}
-                  >
-                    <FacebookIcon className={classes.icon} />
-                  </IconButton>
-                </Tooltip>
-                <Tooltip title="Help us improve on GitHub">
-                  <IconButton
-                    aria-label="contribute on github"
-                    color="inherit"
-                    onClick={openExternalLink(
-                      "https://github.com/ChiliCheese-org/chilicheese.org"
-                    )}
-                  >
-                    <GitHubIcon className={classes.icon} />
-                  </IconButton>
-                </Tooltip>
+    <>
+      <Box className={classes.container}>
+        <Box className={classes.grow}>
+          <AppBar position="static">
+            <Toolbar>
+              <Box className={classes.menuSection}>
+                <Link color="secondary" href="#" onClick={handleClickOpen}>
+                  About
+                </Link>
               </Box>
-            </Box>
-          </Toolbar>
-        </AppBar>
+              <Box className={classes.menuSection}>
+                <Link color="secondary" href="#" onClick={handleClickOpen}>
+                  FAQ
+                </Link>
+              </Box>
+              <Box className={classes.menuSection}>
+                <img
+                  className={classes.logo}
+                  src="images/logo-50.png"
+                  alt="ChiliCheese.org"
+                />
+              </Box>
+              <Box className={classes.menuSection}>
+                <Link color="secondary" href="#" onClick={handleClickOpen}>
+                  Press
+                </Link>
+              </Box>
+              <Box className={classes.menuSection}>
+                <Box className={classes.sectionSocial}>
+                  <Tooltip title="Follow us on Twitter">
+                    <IconButton
+                      aria-label="follow us on twitter"
+                      color="inherit"
+                      onClick={openExternalLink(
+                        "https://twitter.com/ChiliCheese_"
+                      )}
+                    >
+                      <TwitterIcon className={classes.icon} />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="Join us on Facebook">
+                    <IconButton
+                      aria-label="join us on facebook"
+                      color="inherit"
+                      onClick={openExternalLink(
+                        "https://www.facebook.com/TheChiliCheeseBurrito"
+                      )}
+                    >
+                      <FacebookIcon className={classes.icon} />
+                    </IconButton>
+                  </Tooltip>
+                  <Tooltip title="Help us improve on GitHub">
+                    <IconButton
+                      aria-label="contribute on github"
+                      color="inherit"
+                      onClick={openExternalLink(
+                        "https://github.com/ChiliCheese-org/chilicheese.org"
+                      )}
+                    >
+                      <GitHubIcon className={classes.icon} />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+              </Box>
+            </Toolbar>
+          </AppBar>
+        </Box>
       </Box>
-    </Box>
+      <Dialog open={openDialog} onClose={handleClose} scroll="paper">
+        <DialogTitle id="scroll-dialog-title">About</DialogTitle>
+        <DialogContent dividers>
+          <DialogContentText
+            id="scroll-dialog-description"
+            ref={descriptionElementRef}
+            tabIndex={-1}
+          >
+            {[...new Array(50)]
+              .map(
+                () => `Cras mattis consectetur purus sit amet fermentum.
+Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
+Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
+              )
+              .join("\n")}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Close
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
   );
 }
