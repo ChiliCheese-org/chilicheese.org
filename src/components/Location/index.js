aa9f9af3cbd35221e9f59b8d8f6804deadcfcfd1
Steve Gomez <StevenAGomez@Live.com>
2020-08-30 16:02:29 -0700

    Adds VotingPanel stubs

--- src/components/Location/index.js
+++ src/components/Location/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import Tooltip from "@material-ui/core/Tooltip";
 import Typography from "@material-ui/core/Typography";
-import ThumbDownIcon from "@material-ui/icons/ThumbDownRounded";
-import ThumbUpIcon from "@material-ui/icons/ThumbUpRounded";
 import amber from "@material-ui/core/colors/amber";
+import VotingPanel from "../VotingPanel";
 import { distance } from "../../helpers/geo";
 
 const useStyles = makeStyles((theme) => ({
@@ -42,14 +39,6 @@ const useStyles = makeStyles((theme) => ({
   },
   distance: {
     flexBasis: "50px",
-  },
-  votingContainer: {
-    display: "flex",
-  },
-  upVote: { flex: "1", paddingLeft: theme.spacing(1) },
-  downVote: { flex: "1", paddingRight: theme.spacing(1) },
-  voteButton: {
-    width: "100%",
   },
 }));
 
@@ -87,22 +76,7 @@ const Location = ({
             </Box>
           </Box>
 
-          <Box className={classes.votingContainer}>
-            <Box className={classes.downVote}>
-              <Tooltip title="Does not have the chili cheese burrito">
-                <Button className={classes.voteButton} variant="outlined">
-                  <ThumbDownIcon />
-                </Button>
-              </Tooltip>
-            </Box>
-            <Box className={classes.upVote}>
-              <Tooltip title="Has the chili cheese burrito">
-                <Button className={classes.voteButton} variant="outlined">
-                  <ThumbUpIcon />
-                </Button>
-              </Tooltip>
-            </Box>
-          </Box>
+          <VotingPanel location={location} />
         </Box>
       </Box>
     </Box>
